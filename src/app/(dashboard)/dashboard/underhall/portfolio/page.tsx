f8d80bcde69d5110ccbe62e3e15f4c9926cfcc1f
youseffakhro1985 <youseffakhro1985@gmail.com>
2026-07-14 08:47:34 -0700

    Add active maintenance portfolio page

---
+++ src/app/(dashboard)/dashboard/underhall/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import { MaintenancePortfolioPanel } from "@/components/properties/maintenance-portfolio-panel";
+
+export default function MaintenancePortfolioPage() {
+  return (
+    <div className="space-y-8">
+      <MaintenancePortfolioPanel />
+    </div>
+  );
+}
