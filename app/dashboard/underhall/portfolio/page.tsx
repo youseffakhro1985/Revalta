0775c7bf25830904fbe7ffde3071fd0f2b3ab5eb
youseffakhro1985 <youseffakhro1985@gmail.com>
2026-07-14 08:48:18 -0700

    Remove duplicate portfolio page outside active app

--- app/dashboard/underhall/portfolio/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MaintenancePortfolioPanel } from "@/components/properties/maintenance-portfolio-panel";
-
-export default function MaintenancePortfolioPage() {
-  return (
-    <div className="space-y-8">
-      <MaintenancePortfolioPanel />
-    </div>
-  );
-}
